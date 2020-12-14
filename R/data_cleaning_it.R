d3b11bbac0a5c84e4c280c51ef1ae0a99ffaaa3c
laurabalasso <laurabalasso96@gmail.com>
2020-12-14 12:50:06 +0100

    risk level dummies

--- R/data_cleaning_it.R
+++ R/data_cleaning_it.R
@@ -8,7 +8,7 @@ library(ramify)
 ###   total = tamponi     ###
 #############################
 
-## data_it <- read_csv('../data/data_regions.csv')
+## data_it <- read.csv('../data/data_regions.csv')
 
 data_it <- read.csv(url("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv"))
 
@@ -64,5 +64,93 @@ colnames(data_it_ct)[colnames(data_it_ct) == 'data'] <- 'date'
 colnames(data_it_ct)[colnames(data_it_ct) == 'nuovi_positivi'] <- 'positive'
 colnames(data_it_ct)[colnames(data_it_ct) == 'casi_testati'] <- 'total'
 
+
+
+
+### From 2020-11-06 regions grouped in 3 areas based on risk level 
+## (yellow = medium , orange = medium-high , red = high)
+
+d1 <- as.Date('2020-11-06')
+
+r_d1 <- c('Calabria', 'Lombardia', 'Piemonte', 'Valle d\'Aosta')
+o_d1 <-c('Puglia', 'Sicilia')
+y_d1 <- c('Abruzzo', 'Basilicata', 'Campania', 'Emilia-Romagna', "Friuli Venezia Giulia",
+                  'Lazio', 'Liguria','Marche', 'Molise', 'P.A. Bolzano', 'P.A. Trento', 'Sardegna',
+                  'Toscana', 'Umbria', 'Veneto')
+
+
+d2 <- as.Date('2020-11-11')
+r_d2 <- c('P.A. Bolzano')
+o_d2 <- c('Abruzzo', 'Basilicata', 'Liguria' , 'Toscana', 'Umbria' )
+y_d2 <- c()
+
+d3 <- as.Date('2020-11-15')
+r_d3<-c('Campania', 'Toscana')
+o_d3 <- c('Emilia-Romagna', 'Friuli Venezia Giulia', 'Marche')
+y_d3 <- c()
+
+d4 <- as.Date('2020-11-22')
+r_d4 <- c('Abruzzo')
+o_d4 <- c()
+y_d4 <- c()
+
+
+d5 <- as.Date('2020-11-29')
+y_d5 <- c('Liguria', 'Sicilia')
+o_d5 <- c('Calabria', 'Lombardia' , 'Piemonte')
+r_d5 <- c()
+
+d6 <- as.Date('2020-12-06')
+y_d6 <- c('Emilia-Romagna', 'Friuli Venezia Giulia', 'Marche', 'Puglia', 'Umbria' )
+o_d6 <- c('Campania', 'Toscana', 'Valle D\'Aosta' , 'P.A. Bolzano')
+r_d6 <- c()
+
+ordinanze <- c(d1, d2, d3, d4, d5, d6)
+in_yellow <- list(y_d1, y_d2, y_d3, y_d4, y_d5, y_d6)
+in_orange <- list(o_d1, o_d2, o_d3, o_d4, o_d5, o_d6)
+in_red <-    list(r_d1, r_d2, r_d3, r_d4, r_d5, r_d6)
+
+
+## generation of a matrix for each level of risk 
+## each column refers to a region
+## takes value 1 if the region has that level of risk on that date (0 otherwise)
+
+Y<-O<-R <- data.frame(date=unique(data_it$date[data_it$date >= '2020-11-06']))
+
+for(r in unique(data_it$region)){
+  Y[r] <- O[r] <- R[r] <- rep(0, nrow(Y))
+}
+
+
+for(r in 1:length(unique(data_it$region))){
+  reg <- unique(data_it$region)[r]
+  for(i in 1:length(ordinanze)){
+    if(reg %in% unlist(in_yellow[i]))
+      Y[which(Y$date >= ordinanze[i]), r+1] <- rep(1, length(which(Y$date >= ordinanze[i])))
+    else if(reg %in% unlist(in_orange[i]) | reg %in% unlist(in_red[i]))
+      Y[which(Y$date >= ordinanze[i]), r+1] <- rep(0, length(which(Y$date >= ordinanze[i])))
+  }
+}
+
+
+for(r in 1:length(unique(data_it$region))){
+  reg <- unique(data_it$region)[r]
+  for(i in 1:length(ordinanze)){
+    if(reg %in% unlist(in_orange[i]))
+      O[which(O$date >= ordinanze[i]), r+1] <- rep(1, length(which(O$date >= ordinanze[i])))
+    else if(reg %in% unlist(in_yellow[i]) | reg %in% unlist(in_red[i]))
+      O[which(O$date >= ordinanze[i]), r+1] <- rep(0, length(which(O$date >= ordinanze[i])))
+  }
+}
+
+for(r in 1:length(unique(data_it$region))){
+  reg <- unique(data_it$region)[r]
+  for(i in 1:length(ordinanze)){
+    if(reg %in% unlist(in_red[i]))
+      R[which(R$date >= ordinanze[i]), r+1] <- rep(1, length(which(R$date >= ordinanze[i])))
+    else if(reg %in% unlist(in_yellow[i]) | reg %in% unlist(in_orange[i]))
+      R[which(R$date >= ordinanze[i]), r+1] <- rep(0, length(which(R$date >= ordinanze[i])))
+  }
+}
 
 
